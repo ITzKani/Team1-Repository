68fadc22fb233e236b17f09475326f367dd22ae1
Alfredo Diaz <patrik.17089@gmail.com>
2024-10-21 00:19:47 -0600

    V2

--- LADP/Pilas.java
+++ LADP/Pilas.java
@@ -1,16 +1,16 @@
 import java.util.Stack;
 class Pilas{
     public static void main(String[] args) {
-        Stack<Integer> pilap = new Stack<Integer>();
-        Stack<Integer> pilai = new Stack<Integer>();
-        for(int i = 0; i<=30; i++){
-            if(i%2==0){
-            pilap.push(i);
+        Stack<Integer> pilap = new Stack<Integer>();//Abrimos por la pila par
+        Stack<Integer> pilai = new Stack<Integer>();//Abrimos la segunda pila 
+        for(int i = 0; i<=30; i++){//Un for del cero al 30
+            if(i%2==0){//La condicion if del numero par
+            pilap.push(i);//Agregamos a la pilap
         }else{
-            pilai.push(i);
+            pilai.push(i);//agregamos a la pilai
         }
     }
         System.out.println(pilap);
-        System.out.println(pilai);
+        System.out.println(pilai);//Se hacen las impresiones
 }
 }
